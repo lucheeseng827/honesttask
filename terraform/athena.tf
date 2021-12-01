42be3f8d6d5916ec0f30c24ae0372405d05bf7c1
Nicholas Lu Chee Seng <lu.cheeseng827@gmail.com>
2021-12-02 01:53:40 +0700

    Update athena.tf

--- terraform/athena.tf
+++ terraform/athena.tf
@@ -30,7 +30,7 @@
 resource "aws_athena_named_query" "ma50-query-2007" {
   name      = "ma50-query-2007"
   workgroup = "primary"
-  database  = "data"
+  database  = "default"
   query     = "select *,avg(data.risk_score) over(order by data.\"application date\" rows between 50 preceding and current row) as RiskScoreMA50 from data where data.\"application date\" like '2007%';"
 }
 
@@ -38,7 +38,7 @@ resource "aws_athena_named_query" "ma50-query-2007" {
 resource "aws_athena_named_query" "ma50-query-2008-2009" {
   name      = "ma50-query-2008-2009"
   workgroup = "primary"
-  database  = "data"
+  database  = "default"
   query     = "select *,avg(data.risk_score) over(order by data.\"application date\" rows between 50 preceding and current row) as RiskScoreMA50ST from data where data.\"application date\" between '2008-01-01' and '2009-12-31';"
 }
 
@@ -47,7 +47,7 @@ resource "aws_athena_named_query" "ma50-query-2008-2009" {
 resource "aws_athena_named_query" "ma100-query" {
   name      = "ma100-query"
   workgroup = "primary"
-  database  = "data"
+  database  = "default"
   query     = "select *,avg(data.risk_score) over(order by data.\"application date\" rows between 50 preceding and current row) as RiskScoreMA100 from data where data.\"application date\" > '2009-01-01';"
 }
 
@@ -55,6 +55,6 @@ resource "aws_athena_named_query" "ma100-query" {
 resource "aws_athena_named_query" "ema50-query" {
   name      = "ema50-query"
   workgroup = "primary"
-  database  = "data"
+  database  = "default"
   query     = "select *,avg(data.risk_score) over(order by data.\"application date\" rows between 50 preceding and current row) as RiskScoreEMA50 from data where data.\"application date\" like '2007%';"
 }
